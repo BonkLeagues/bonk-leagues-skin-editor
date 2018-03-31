4c260f83378ba6e460fe565f532cba00d2437c62
Finbae <finlay@sebweb.co.uk>
2018-03-31 11:16:34 +0100

    Added delete button, new build (also updated on BL)

--- js/components/Options/AllInputs.js
+++ js/components/Options/AllInputs.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import OptionsInput from './OptionsInput';
 import OptionsCheckbox from './OptionsCheckbox';
 
-export default () => (
+var AllInputs = ({ deleteLayer }) => (
     <div>
         <OptionsInput type="scale" />
         <OptionsInput type="rotation" />
@@ -14,5 +15,27 @@ export default () => (
             <OptionsCheckbox type="hf" />
             <OptionsCheckbox type="vf" />
         </div>
+
+        <div className="buttons">
+            <button className="delete-layer" onClick={()=>deleteLayer()}>
+                Delete Shape
+            </button>
+        </div>
     </div>
 );
+
+var mapStateToProps = (state, props) => {
+    return {};
+}
+var mapDispatchToProps = (dispatch, props) => {
+    return {
+        deleteLayer: () => {
+            dispatch({type: 'DELETE_SELECTED_SHAPE'});
+        }
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AllInputs);
