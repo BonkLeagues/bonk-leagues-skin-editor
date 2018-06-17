aa0b33ef13533f1313f00344c99963cffb4905ae
WheatleyTheCoder <slendermanwithaface@gmail.com>
2018-06-17 16:34:30 +0100

    Cleaned up some code

--- src/js/components/SkinBase/OverlayPanel.js
+++ src/js/components/SkinBase/OverlayPanel.js
@@ -7,27 +7,30 @@ import Color from '../Color';
 
 var OverlayPanel = ({ active, palette, src, onActiveChange, onSrcChange, onScaleChange }) => (
     <div className="overlay-panel" onMouseDown={e=>e.stopPropagation()}>
-        <div className={src ? 'overlay-panel-on' : 'overlay-panel-off'}>
-            <span>Overlay Controls</span>
-            <div onClick={()=>onActiveChange(active)} className={'checkbox ' + (active ? 'ticked' : '')} />
-            <Slider
-                defaultValue={100}
-                min={25}
-                max={300}
-                onChange={onScaleChange}
-            />
-            <div className="palette">
-                {
-                    palette &&
-                    palette.map((color, i) =>
-                        <Color color={color} key={i} />
-                    )
-                }
+        {
+            src ?
+            <div>
+                <span>Overlay Controls</span>
+                <div onClick={()=>onActiveChange(active)} className={'checkbox ' + (active ? 'ticked' : '')} />
+                <Slider
+                    defaultValue={100}
+                    min={25}
+                    max={300}
+                    onChange={onScaleChange}
+                />
+                <div className="palette">
+                    {
+                        palette &&
+                        palette.map((color, i) =>
+                            <Color color={color} key={i} />
+                        )
+                    }
+                </div>
+            </div> :
+            <div>
+                <span>Drag-and-drop an image here to enable the image overlay</span>
             </div>
-        </div>
-        <div className={src ? 'overlay-panel-off' : 'overlay-panel-on'}>
-            <span>Drag-and-drop an image here to enable the image overlay</span>
-        </div>
+        }
     </div>
 );
 
