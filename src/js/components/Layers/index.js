388ebb2eba65fdc7c65dca7856d67b066db865b8
WheatleyTheCoder <bonk.its.magic@gmail.com>
2018-06-22 14:49:31 +0100

    Added scrollbar to layers panel

--- src/js/components/Layers/index.js
+++ src/js/components/Layers/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import Layer from './layer';
 
@@ -24,10 +25,29 @@ export default class Layers extends React.Component {
                     right: this.state.visible ? '0' : '-260px'
                 }}
             >
-                <Layer />
-                <Layer />
-                <Layer />
-                <Layer />
+                <Scrollbars
+                    style={{
+                        width: '100%',
+                        height: '100%'
+                    }}
+                    renderTrackVertical={props => <div {...props} className="track-vertical" />}
+                    renderThumbVertical={props => <div {...props} className="thumb-vertical" />}
+                >
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                    <Layer />
+                </Scrollbars>
                 <img className="arrow"
                     onClick={this.toggleVisible}
                     src={require(this.state.visible ? './icons/arrow_flipped.svg' : './icons/arrow.svg')}
