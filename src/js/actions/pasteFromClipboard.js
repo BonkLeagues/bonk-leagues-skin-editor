7cf116b1566b23a614feb55967d2486b8f371821
WheatleyTheCoder <bonk.its.magic@gmail.com>
2018-09-08 16:07:58 +0100

    Fixed bug with empty clipboard

--- src/js/actions/pasteFromClipboard.js
+++ src/js/actions/pasteFromClipboard.js
@@ -2,6 +2,9 @@ export default (dispatch, getState) => {
     // Gets the clipboard contents from the Redux state
     var { clipboard } = getState();
 
+    // If there is no shape on the clipboard, do nothing
+    if (!clipboard.name) return;
+
     // Changes the shape's unique identifier
     dispatch({type: 'CHANGE_CLIPBOARD_UUID'});
 
